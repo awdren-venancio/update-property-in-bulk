5875983ddf0c7468d54185b983e62b96d452937c
Awdren Venancio <awdren@MacBook-Pro-de-Awdren-2.local>
2024-09-22 17:30:30 -0300

    Acesso logs

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,26 @@ use App\Http\Controllers\Automation\AutomationController;
 Route::get('start-automation', [AutomationController::class, 'startAutomation']);
 Route::get('property/list', [PropertyController::class, 'listProperty']);
 Route::put('property/update/{id}', [PropertyController::class, 'updateProperty']);
+
+
+// Codigo provisório até ser criado um relatório de logs *********************************
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+Route::get('/logs/35a10dfa8d4ce345c8dad22fb2568b7e', function () {
+    $path = storage_path('logs/laravel.log');
+
+    if (File::exists($path)) {
+        $log = File::get($path);
+
+        if (strlen($log) > 500000) {
+            $log = substr($log, -500000);
+        }
+
+        return Response::make(nl2br($log), 200, [
+            'Content-Type' => 'text/html',
+        ]);
+    }
+
+    return response()->json(['message' => 'Log file not found.'], 404);
+});
+// ******************************************************************************************
